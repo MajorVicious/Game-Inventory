7708d3b63f9e6ed9cd78fc284f576d48d1a978a6
Yann Gazoni <yann.gazoni@gmail.com>
2017-03-28 22:13:08 +0200

    Two new functionalities, staring game & datestamp

--- games.py
+++ games.py
@@ -4,7 +4,8 @@ import yaml
 import os
 import prettytable
 import random
-import proprieties
+import datetime
+
 
 
 @attr.s
@@ -14,6 +15,10 @@ class Game(object):
     max_player = attr.ib()
     genre = attr.ib()
     duration = attr.ib()
+    level = attr.ib()
+    last_played = attr.ib(default=None)
+    stars = attr.ib(default=None)
+
 
 
 @click.group()
@@ -29,6 +34,9 @@ def cli():
     '--max-player', type=int, prompt='Max. number of players', default=4)
 @click.option('--genre', prompt='Game genre')
 @click.option('--duration', type=int, prompt='Game duration (min)', default=5)
+@click.option(
+    '--level', type=str, prompt='Difficulty(Novice, Intermediate, Expert)', 
+    default='Intermediate')
 def add(*args, **kwargs):
     g = Game(*args, **kwargs)
     save(g)
@@ -39,16 +47,17 @@ def add(*args, **kwargs):
 def search(player, time):
     candidates = []
     inventory = load()
+    
 
     for game in inventory:
-        if game["min_player"] <= player <= game["max_player"] \
-                                and game["duration"] <= time:
+        if (game["min_player"] <= player <= game["max_player"]
+                                and game["duration"] <= time):
             candidates.append(game)
 
 
     if candidates:
         results = []
-        while sum(item["duration"] for item in results) < time:
+        while sum(item["duration"] for item in results) < time and candidates:
             choice = random.choice(candidates)
             if sum(item["duration"] for item in results) + choice["duration"] > time:
                 break
@@ -57,15 +66,16 @@ def search(player, time):
             candidates.remove(choice)
 
         show(results)
+        played(results)
 
 
     else:
         print("There is no game to choose from.")
 
 def load():
-    FILENAME = useProprieties()
-    if os.path.exists(FILENAME):
-        with open(FILENAME, 'r') as f:
+    filename = define_list()
+    if os.path.exists(filename):
+        with open(filename, 'r') as f:
             inventory = yaml.load(f)
             return inventory
     else:
@@ -87,53 +97,91 @@ def list(sort):
     inventory.sort(key=lambda x: x[sort])
     show(inventory)
 
-def save(g):
-    FILENAME = defineList()
+def save(game):
+    filename = define_list()
 
-    if os.path.exists(FILENAME):
-        with open(FILENAME, 'r') as f:
+    if os.path.exists(filename):
+        with open(filename, 'r') as f:
             inventory = yaml.load(f)
     else:
         inventory = []
 
     updated = False
     for existing in inventory:
-        if existing['name'] == g.name:
+        if existing['name'] == game.name:
             updated = True
-            existing.update(g.__dict__)
+            existing.update(game.__dict__)
 
     if not updated:
-        inventory.append(g.__dict__)
+        inventory.append(game.__dict__)
 
-    with open(FILENAME, 'w') as f:
+    with open(filename, 'w') as f:
         yaml.dump(inventory, f)
 
     if updated:
-        print('{} updated'.format(g.name))
+        print('{} updated'.format(game.name))
     else:
-        print('{} added'.format(g.name))
+        print('{} added'.format(game.name))
+
+def update(game):
+    filename = define_list()
+
+    with open(filename, 'r') as f:
+        inventory = yaml.load(f)
+
+    for existing in inventory:
+        if existing["name"] == game["name"]:
+            existing["last_played"] = game["last_played"]
+            existing["stars"] = game["stars"]
+
+
+    with open(filename, 'w') as f:
+        yaml.dump(inventory, f)
+
 
 @cli.command()
 @click.option('--data', prompt="Do you want to use the default list ?")
 def newList(data):
     if data.lower() == 'y' or data == '':
-        FILENAME = str(defineList())
+        filename = define_list()
     elif data.lower() == 'n':
-        FILENAME = input("Enter the name of your game list, with the extension '.yml'")
-        
+        filename = input("Enter the name of your game list, with the extension '.yml'")
 
-    with open('proprieties.txt', 'w') as p:
-        yaml.dump('DEFAULT LIST = {}'.format(FILENAME), p)
+
+    with open('properties.txt', 'w') as p:
+        save = {"Default list": filename}
+        yaml.dump(save, p)
+
+def played(results):
+    temp = []
+    for result in results:
+        play = input("Do you played the game ? ")
+        if play.lower() == "y":
+            temp.append(result)
+            for game in temp:
+                day = datetime.date.today()
+                game["last_played"] = datetime.date.strftime(day, '%d-%m-%Y')
+                star = starred(game)
+                update(star)
+        else:
+            pass
+
+def starred(game):
+    star = input("Do you want to rate the game ? ")
+    if star.lower() == "y":
+        rate = eval(input("How many star ? from 0 to 5 "))
+        game["stars"] = rate
+        return game
+    else:
+        pass
 
 #Configuration file
 
-def defineList():
-    with open('proprieties.txt', 'r') as p:
-        config = yaml.load(p)
-        lecture = config.split()
-        if "LIST" in lecture:
-            FILENAME = config.split('=',1)[1].strip()
-    return FILENAME
+def define_list():
+    with open('properties.txt', 'r') as p:
+        list_used = yaml.load(p)
+        filename = list_used["Default list"]
+    return filename
 
 
 
