566d644da3456d988d15fcb6068fc1028e8e6f44
Yann Gazoni <yann.gazoni@gmail.com>
2017-06-12 20:06:57 +0200

    request method

--- web/download_db.py
+++ web/download_db.py
@@ -1,5 +1,8 @@
 import requests
 import untangle
+import time
+import csv
+import os
 
 class BoardGame:
 	def __init__(self, node):
@@ -26,24 +29,106 @@ class BoardGame:
 	def categories(self):
 		return [x.cdata for x in self._node.boardgamecategory]
 
+	@property
+	def min_player(self):
+		return int(self._node.minplayers.cdata)
 
-	def save(self, *args, **kwargs):
-		pass
+	@property
+	def max_player(self):
+		return int(self._node.maxplayers.cdata)
+	
+	@property
+	def playing_time(self):
+    		return int(self._node.playingtime.cdata)
+	
+	@property
+	def age(self):
+    		return int(self._node.age.cdata)
+	@property
+	def thumbnail(self):
+ 		return self._node.thumbnail.cdata
+
+	@property
+	def rating(self):
+    		return float(self._node.statistics.ratings.average.cdata)
+
+	@property
+	def weight(self):
+		return float(self._node.statistics.ratings.averageweight.cdata)
+
+	@property
+	def description(self):
+		text = self._node.description.cdata
+		return text
+
+	def features(self):
+		return {'name': self.name,
+				'year_published': self.year_published,
+				'mechanics': self.mechanics,
+				'categories': self.categories,
+				'min_player': self.min_player,
+				'max_player': self.max_player,
+				'playing_time': self.playing_time,
+				'age': self.age,
+				'thumbnail': self.thumbnail,
+				'rating': self.rating,
+				'weight': self.weight,
+				'description': self.description}
 
 
-def ids_generator():
-	return list(range(1, 5))
+def save(game):
+	if os.path.exists('inventory.csv'):
+		with open('inventory.csv', 'r') as f:
+			reader = csv.DictReader(f)
+			inventory = []
+			for row in reader:
+				inventory.append(row)
+				
+				
+	else:
+		inventory = []
+
+	updated = False
+	for existing in inventory:
+		if existing['name'] == game.name:
+			updated = True
+			existing.update(game.features())
+
+	if not updated:
+		inventory.append(game.features())
+
+	with open('inventory.csv', 'w') as f:
+		fieldnames = (['name', 'year_published', 'mechanics', 'categories', 'min_player', 
+		'max_player', 'playing_time', 'age', 'thumbnail', 'rating', 'weight', 'description'])
+		writer = csv.DictWriter(f, fieldnames=fieldnames)
+		writer.writeheader()
+		for game in inventory:
+			writer.writerow(game)
+
+def ids_generator(x, y):
+	array = range(x, y)
+	return list(array)
 
 
+def request():
+	z = 0
+	x = 10
+	y = x + 20
+	while z < 5: 
+		for ids in ids_generator(x, y):
+			r = requests.get('https://www.boardgamegeek.com/xmlapi/boardgame/{}?stats=1'.format(ids))
+			doc = untangle.parse(r.text)
+			chunk = [BoardGame(g) for g in doc.boardgames.boardgame]
+			for game in chunk:
+				save(game)
+		x = y
+		y = x + 20
+		z += 1
+		print("Still {} to do.".format(5 - z))
+		time.sleep(2)
 
-for ids in ids_generator():
-	r = requests.get('https://www.boardgamegeek.com/xmlapi/boardgame/{}?stats=1'.format(ids))
-	doc = untangle.parse(r.text)
-	chunk = [BoardGame(g) for g in doc.boardgames.boardgame]
-	for game in chunk:
-		print(game.name, game.year_published, game.mechanics, game.categories)
-		game.save()
 
+request()
 
 
 
